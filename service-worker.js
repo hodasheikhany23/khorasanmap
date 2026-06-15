37290102d66d67da4ad8bf856d1f1adea0c21770
Hoda Sheikhany <1385.hoda.sh@gmail.com>
2026-06-15 07:24:39 +0330

    add maifest and service-worker

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "khorasan-Yadman-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./adminarea.js",
+  "./places_khorasan.js",
+  "./points.js",
+  "./road.js",
+  "./manifest.json",
+  "./service-worker.js",
+  "./assets/css/style.css", 
+  "./assets/css/bootstrap.rtl.min.css",
+  "./assets/css/bootstrap.rtl.min.css.map", 
+  "./assets/fonts/IRANSansX-RegularD4.woff2",
+  "./assets/js/main.js",
+  "./assets/js/bootstrap.bundle.min.js",
+  "./assets/js/bootstrap.min",
+  "./assets/leaflet/leaflet.js",
+  "./assets/leaflet/leaflet.css"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
